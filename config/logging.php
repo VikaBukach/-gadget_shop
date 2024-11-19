cf47252f666a88045dc08b6c3a92fe7de2041fdd
vikabukach <bukachviko@gmail.com>
2024-11-19 10:27:40 +0200

    fix

--- config/logging.php
+++ config/logging.php
@@ -133,8 +133,8 @@ return [
             'driver' => 'custom',
             'via' => TelegramLoggerFactory::class,
             'level' =>  'debug',
-            'chat_id' => env('TELEGRAM_BOT_TOKEN'),
-            'token' => env('TELEGRAM_CHAT_ID'),
+            'chat_id' => env('TELEGRAM_CHAT_ID'),
+            'token' => env('TELEGRAM_BOT_TOKEN'),
         ],
 
     ],
